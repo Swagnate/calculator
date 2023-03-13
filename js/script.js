051a6536251c50dae6f269cf52c0b8caa9b6805f
Matveev matvei <matthew.marinara@gmail.com>
2023-03-13 08:08:59 +0300

    added ac function

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ const board = document.querySelector('.input-screen');
 const buttons = document.querySelector('.buttons');
 
 // массивы 
-const controllers = ["AC", "+/-", "%", "/", "x", "-", "+", "=", ","]
+const controllers = ["AC", "+/-", "%", "/", "x", "-", "+", ","]
 const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ","]
 
 // переменные
@@ -19,50 +19,44 @@ function clearALL() {
     num2 = ''
     operation = ''
     finish = false
-    board.textContent = 0
+    board.value = 0
+}
+clearALL();
+
+const cleaner = document.querySelector('#clear')
+cleaner.onclick = function () {
+    num1 = ''
+    num2 = ''
+    operation = ''
+    finish = false
+    board.value = 0
 }
 
-document.querySelector('.ac').addEventListener = clearALL;
+buttons.addEventListener('click', (event) => {
 
-document.querySelector('buttons').addEventListener = (event) => {
-    // нажата кнопка
-    if (!event.target.classList.contains('btn')) return;
-    // нажата кнопка clearALL ac
-    if (!event.target.classList.contains('ac')) return;
+    const contex = event.target.textContent
 
-    board.textContent = '';
-    // получаем нажатую кнопку
-    const key = event.target.textContent;
-
-    // если нажата клавиши 0-9 или ,
-    if (numbers.includes(key)) {
-        if (num2 === '' && operation === '') {
-            num1 += key;
-
-            board.textContent = num1;
+    if (numbers.includes(contex)) {
+        if (operation === '') {
+            num1 += +contex;
+            board.value = num1;
         }
-        else if (num1 !== '' && num2 !== '' && finish) {
-            num2 = key;
-            finish = false;
-            board.textContent = num2;
+        if (operation !== '') {
+            num2 += +contex;
+            board.value = num2;
         }
-        else {
-            num2 += key;
-            board.textContent = num2;
-        }
-        return;
+
+
     }
 
     // если нажата клавиша + - / x
-    if (controllers.includes(key)) {
-        operation = key;
-        board.textContent = operation;
-        return;
+    if (controllers.includes(contex)) {
+        operation = contex;
+
     }
 
-    // нажата =
-    if (key === "=") {
-        if (num2 === '') num2 = num1;
+    if (contex === "=") {
+
         switch (operation) {
             case "+":
                 num1 = (+num1) + (+num2);
@@ -78,7 +72,14 @@ document.querySelector('buttons').addEventListener = (event) => {
                 break;
         }
         finish = true;
-        board.textContent = num1;
+        num2 = 0
+        operation = ''
+        board.value = num1;
 
     }
-}
+    console.log(`num1:${num1}`)
+    console.log(`num2:${num2}`)
+    console.log(`operation:${operation}`)
+})
+
+
